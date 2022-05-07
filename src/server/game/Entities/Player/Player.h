b2be133495811cbecfbd1697a6fa3a60e917ddab
ariel- <ariel-@users.noreply.github.com>
2022-05-07 15:52:56 +0200

    Core/Entities: Port some refactors from f1986c6aafdf2457902a7a4cc7acc903cbbdc7bb

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2022,6 +2022,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
 
         void ResetAllPowers();
 
+        SpellSchoolMask GetMeleeDamageSchoolMask(WeaponAttackType attackType = BASE_ATTACK) const override;
+
         void CastAllObtainSpells();
         void ApplyItemObtainSpells(Item* item, bool apply);
 
